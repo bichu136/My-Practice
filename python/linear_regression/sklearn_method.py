985c57d03c04740e457e76ebcb9696a631f253e4
bichu136 <bicagu123@gmail.com>
2021-08-25 01:54:02 +0700

    fix for dodi

--- python/linear_regression/sklearn_method.py
+++ python/linear_regression/sklearn_method.py
@@ -7,8 +7,12 @@ def read_data(filename):
     with open(filename) as file:
         line =file.readline() 
         while(line!=""):
-            line+= ",1"
-            r.append([float(i) for i in line.split(",")])
+            line= line.strip() +",1"
+            print(line.split(','))
+            k = 1
+            for i in line.split(","):
+                k = k*float(i)
+            r.append([k,1])
             line = file.readline()
     return np.array(r,dtype=np.float64)
 def read_data_y(filename):
@@ -17,16 +21,18 @@ def read_data_y(filename):
         line =file.readline() 
         while(line!=""):
             # line+= ",1"
+            line = line.strip()
             r.append([float(i) for i in line.split(",")])
             line = file.readline()
     return np.array(r,dtype=np.float64)
 # Load the diabetes dataset
-n = read_data('./linear_regression/n_mergesearch..txt')
+
+n = read_data('1.txt')
 nlgn = np.transpose(np.vstack(((n[:,0])*np.log2(n[:,0]),n[:,1])))
 lgn = np.transpose(np.vstack((np.log2(n[:,0]),n[:,1])))
 nn = np.transpose(np.vstack(((n[:,0])*(n[:,0]),n[:,1])))
 sqrt_n = np.transpose(np.vstack((np.sqrt(n[:,0]),n[:,1])))
-y = read_data_y('./linear_regression/y_mergesearch..txt')
+y = read_data_y('2.txt')
 def result(x,y,str):
     # Create linear regression object
     regr = linear_model.LinearRegression()
@@ -40,8 +46,8 @@ def result(x,y,str):
     # The mean squared error
     print('Mean squared error of '+ str +': %.6f'
         % mean_squared_error(y, y_pred))
-result(n,y,"n")
-result(nlgn,y,"nlgn")
-result(nn,y,"nn")
-result(lgn,y,"lgn")
-result(sqrt_n,y,"sqrt_n")
+result(n,y,"$n$")
+result(nlgn,y,"$nlog(n)$")
+result(nn,y,"$n^2$")
+result(lgn,y,"$log(n)$")
+result(sqrt_n,y,"$\sqrt{n}$")
